2eb2768c28c83a5bbf186320683208dd3a8ec575
KerLq <oussamagaming2001@outlook.de>
2021-08-08 19:29:48 +0200

    Add validations and attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    validates_presence_of :password, :on => :create
+    attr_accessible :name, :email, :password, :password_confirmation
 end
