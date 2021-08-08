08b79778fe132a28237fc60a9392c13d083883d1
KerLq <oussamagaming2001@outlook.de>
2021-08-08 19:37:39 +0200

    Add session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def create
+        @user = User.find_by(username: params[:username])
+
+        if !!@user && @user.authenticate(params[:username])
+
+            session[:user_id] = @user.id
+            #redirect_to {homescreen or latest page}
+
+        else
+            message = "Login is invalid!"
+            #redirect_to {login page}, notice: message
+            
+        end
+    end
+
+    def destroy
+        session.delete(:user_id)
+    end
+
+end
