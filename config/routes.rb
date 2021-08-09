4367f094669c5d50364aead86e4e1cbabffb923e
KerLq <oussamagaming2001@outlook.de>
2021-08-09 05:10:51 +0200

    Rename index to register (routes leads to register method in sessions controller)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homes#index'
 
-  get '/register', to: 'sessions#index'
+  get '/register', to: 'sessions#register'
   post '/register', to: 'sessions#create'
+
 
 end
