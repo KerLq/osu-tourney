f51e64b37d5dd406e1aeb604b79d8f04e65b6e99
kerlq <oussamagaming2001@outlook.de>
2021-10-17 22:50:33 +0200

    Accessing the elements in json

--- app/controllers/parses_controller.rb
+++ app/controllers/parses_controller.rb
@@ -1,12 +1,28 @@
 class ParsesController < ApplicationController 
     def parse 
-        url = "https://osu.ppy.sh/community/matches/89309929"
-        @unparsed_page = HTTParty.get(url)
-        parsed_page = Nokogiri::HTML(@unparsed_page)
+        url = "https://osu.ppy.sh/community/matches/92550524"
+        unparsed_page = HTTParty.get(url)
+        parsed_page = Nokogiri::HTML(unparsed_page)
         parsed_page = parsed_page.css('script')[11]
         #@parsed_json = ActiveSupport::JSON.decode(@parsed_page)
         #parsed_page.gsub!(/\"/, '\'')
-        parsed_page = JSON.parse(parsed_page)
+        @json = JSON.parse(parsed_page)
+        
+        # i = 0
+        # loop do 
+        #     i+=1
+        #     break if @json['events'][i].keys.include?('game')
+        # end
+
+        x = 0
+        scores = {}
+        for h in @json['events'] do
+            if h.has_key? 'game'
+                x += 1
+            end
+        end
+        # score = @json['events'][i]
+                                                            #@json['events'][42]['game']['scores'][0]['score']
         debugger
     end
 
