2dfc2af5e59b7cd1fcad8ff99a15981e0fdda858
kerlq <oussamagaming2001@outlook.de>
2021-10-21 16:43:52 +0200

    Remove tourney model

--- db/migrate/20211021143409_create_tourneys.rb
+++
@@ -1,8 +0,0 @@
-class CreateTourneys < ActiveRecord::Migration[6.1]
-  def change
-    create_table :tourneys do |t|
-
-      t.timestamps
-    end
-  end
-end
--- test/models/tourney_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class TourneyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
