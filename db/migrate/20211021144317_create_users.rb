79ecf393ccc56a63a68c3e96cf3bbe1ee0ded24f
kerlq <oussamagaming2001@outlook.de>
2021-10-21 16:43:35 +0200

    Remove User model to generate migration via scaffold

--- db/migrate/20211021144317_create_users.rb
+++ db/migrate/20211021144317_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :avatar_url
+      t.int :user_id
 
       t.timestamps
     end
