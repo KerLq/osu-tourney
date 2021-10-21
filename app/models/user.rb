4c297d151dc4a90009ef4fa3c99d7068cb103139
kerlq <oussamagaming2001@outlook.de>
2021-10-21 16:28:58 +0200

    Destroy user model

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    has_secure_password
-    validates :password, length: { minimum: 3 }, if: :password_digest_changed?
-    #validates_presence_of :password, :on => :create
-    #attr_accessible :username, :email, :password, :password_confirmation
-end
