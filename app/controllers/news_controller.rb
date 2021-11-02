ea7ce0372306f5fcf36d118430427c404eb242d8
kerlq <oussamagaming2001@outlook.de>
2021-11-02 13:10:39 +0100

    Add basic methods in news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,32 @@
 class NewsController < ApplicationController
     before_action :check_if_admin?, :only => [:new, :update, :destroy]
 
+    def index
+        @news = News.all
+    end
+    def show
+        @news = News.find(id: params[:id])
+    end
+
+    def update
+        @news = News.find(id: params[:id])
+
+        @news.update(news_params)
+    end
+
+    def destroy
+
+    end
+    private
     def check_if_admin?  
         redirect_to root_path if !is_admin?
+    end
+
+    def news_params
+        params.permit(:news).require (
+            :title,
+            :description,
+            :cover_image
+        )
     end
 end
