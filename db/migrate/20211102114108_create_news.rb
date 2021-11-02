eb7d2e7a2da4addf3178511c2a68d69c934cef8e
kerlq <oussamagaming2001@outlook.de>
2021-11-02 12:43:09 +0100

    Add cover_image to table (news)

--- db/migrate/20211102114108_create_news.rb
+++ db/migrate/20211102114108_create_news.rb
@@ -3,7 +3,8 @@ class CreateNews < ActiveRecord::Migration[6.1]
     create_table :news do |t|
       t.string :title
       t.string :description
-
+      t.string :cover_image
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_114108) do
   create_table "news", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.string "cover_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
