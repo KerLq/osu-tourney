a0d6230816160a4389bf4e06a988760554ae2085
kerlq <oussamagaming2001@outlook.de>
2021-11-02 12:50:13 +0100

    implemented is_admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user
     helper_method :logged_in?
+    helper_method :is_admin?
   
     def current_user
         if session[:user_id]
@@ -11,6 +12,10 @@ class ApplicationController < ActionController::Base
     end
   
     def logged_in?    
-      session[:user_id] != nil
+        !!current_user
+    end
+
+    def is_admin?
+        logged_in? current_user.admin? : false
     end
 end
---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,9 @@
+class NewsController < ApplicationController
+    helper_method :is_admin?
+
+
+
+    def check_if_user_is_admin
+
+    end 
+end
