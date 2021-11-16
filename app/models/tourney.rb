242cfcf874b6c5be875b9a2826af0464b29520ea
kerlq <oussamagaming2001@outlook.de>
2021-11-16 15:53:58 +0100

    Add random number in url for tourneys

--- app/models/tourney.rb
+++ app/models/tourney.rb
@@ -1,4 +1,16 @@
 class Tourney < ApplicationRecord
     belongs_to :user
     has_many :matches
+    before_create :randomize_id
+
+
+    private
+    def randomize_id
+        begin
+            number = SecureRandom.random_number
+            number = number.to_s.gsub(/\./mi, '')
+            self.id = number
+        end while Tourney.where(id: self.id).exists?
+    end
+
 end
