5cb37d11c6f85e9e9a2caf2f7d285ef7df582c45
kerlq <oussamagaming2001@outlook.de>
2021-11-16 12:33:15 +0100

    Remove unnessecary code

--- config/application.rb
+++ config/application.rb
@@ -10,19 +10,7 @@ module OsuTourney
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    Rails.application.config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        if Rails.env.development?
-          origins 'localhost:3000', 'localhost:3001', 'https://osu.ppy.sh/community/matches/89309929'
-        else
-          origins 'https://osu.ppy.sh/community/matches/89309929'
-        end
-    
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      end
-    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
