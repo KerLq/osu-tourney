0c9bf960d8da56735bdec127cc8546edc5b98b38
kerlq <oussamagaming2001@outlook.de>
2022-02-10 20:00:00 +0100

    Remove setApi method in frontend controller

--- app/controllers/frontend/frontend_controller.rb
+++ app/controllers/frontend/frontend_controller.rb
@@ -2,13 +2,6 @@ class Frontend::FrontendController < ApplicationController
 
     @@osuApi = nil
 
-    # def setApi
-    #     if session[:access_token] && @@osuApi.nil?
-    #         @@osuApi = Osu::Api::Api.new(session[:access_token])
-    #     end
-    #     debugger
-    # end
-
     def osuApi
         @@osuApi ||= Osu::Api::Api.new(session[:access_token]) if session[:access_token]
     end
