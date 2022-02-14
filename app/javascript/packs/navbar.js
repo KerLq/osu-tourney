1a08a20b75ee01b63a03aa62d291b93415ebc47e
kerlq <oussamagaming2001@outlook.de>
2022-02-14 01:01:50 +0100

    Added hamburger menu rotate animation and logout button on mobile in navbar

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -6,8 +6,23 @@ window.onload = function() {
     // button.addEventListener("click", () => {
     //     div.classList.toggle("active");
     // });
-
+    $burger = $(".navbar__hamburger");
+    $links = $(".navbar__links");
     $(".navbar__hamburger").click(function() {
-        $(".navbar__links").toggleClass("navbar__links--active");
+        $links.toggleClass("navbar__links--active");
+
+        if ($burger.hasClass("navbar__hamburger--animation") && !$burger.hasClass("navbar__hamburger--reverse-animation")) {
+            $burger.toggleClass("navbar__hamburger--animation");
+            $burger.addClass("navbar__hamburger--reverse-animation");
+            return;
+        } else if ($burger.hasClass("navbar__hamburger--reverse-animation")) {
+            $burger.toggleClass("navbar__hamburger--reverse-animation");
+            $burger.addClass("navbar__hamburger--animation");
+
+            return;
+        }
+        $burger.toggleClass("navbar__hamburger--animation");
+
+        
     });
 }
